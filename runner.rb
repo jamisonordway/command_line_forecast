1a880288cd1651468a31cb92ca9953283d79ef6b
JamisonOrdway <jamisonordway@gmail.com>
2018-11-05 22:07:51 -0700

    Adds examples to parser

--- runner.rb
+++ runner.rb
@@ -5,16 +5,16 @@ require './file_write.rb'
 options = {}
 
 OptionParser.new do |parser|
-  parser.on('-a', '--latitude LATITUDE', Float, 'The latitude for the forecast') do |v|
+  parser.on('-a', '--latitude LATITUDE', Float, 'The latitude for the forecast. Example: 37.7749') do |v|
     options[:latitude] = v
   end
-  parser.on('-o', '--longitude LONGITUDE', Float, 'The longitude for the forecast') do |v|
+  parser.on('-o', '--longitude LONGITUDE', Float, 'The longitude for the forecast. Example: 122.4194') do |v|
     options[:longitude] = v
   end
-  parser.on('-g', '--granularity GRANULARITY', String, 'The granularity for the forecast results') do |v|
+  parser.on('-g', '--granularity GRANULARITY', String, "The granularity for the forecast results. Example: 'currently', 'daily', or 'hourly'") do |v|
     options[:granularity] = v
   end
-  parser.on('-p', '--path PATH', String, 'The optional filepath for the forecast results') do |v|
+  parser.on('-p', '--path PATH', String, 'The optional filepath for the forecast results.') do |v|
     options[:path] = v
   end
 end.parse!
